d6c2c9f3aa6c08d7342f6a8228fcfebd83892c8b
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-02-22 13:21:41 +0100

    Explicitly allow LB to send traffic to application
    Previously we only did it the other way. The Vy LB module has
    removed the blanket egress permission to increase security posture,
    so it is needed here now.

--- main.tf
+++ main.tf
@@ -118,12 +118,30 @@ resource "aws_security_group_rule" "loadbalancer" {
     : { for lb in var.lb_listeners : lb.listener_arn => lb.security_group_id }
   )
 
-  security_group_id        = aws_security_group.ecs_service[0].id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = var.application_container.port
-  to_port                  = var.application_container.port
+  security_group_id = aws_security_group.ecs_service[0].id
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = var.application_container.port
+  to_port   = var.application_container.port
+
   source_security_group_id = each.value
+}
+
+resource "aws_security_group_rule" "loadbalancer_to_service" {
+  for_each = (var.launch_type == "EXTERNAL"
+    ? {}
+    : { for lb in var.lb_listeners : lb.listener_arn => lb.security_group_id }
+  )
+
+  security_group_id = each.value
+
+  type      = "egress"
+  protocol  = "tcp"
+  from_port = var.application_container.port
+  to_port   = var.application_container.port
+
+  source_security_group_id = aws_security_group.ecs_service[0].id
 }
 
 resource "aws_security_group_rule" "egress_service" {
