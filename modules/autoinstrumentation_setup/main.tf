ce3271b28d63d14d46940ee597011d981fa191fb
Nicolas Harlem Eide <nicolas@harlemeide.net>
2024-09-04 13:52:28 +0200

    Fuck it

--- modules/autoinstrumentation_setup/main.tf
+++ modules/autoinstrumentation_setup/main.tf
@@ -79,23 +79,23 @@ locals {
 output "application_container_definition" {
   value = local.new_definition
 
-  precondition {
-    # Error if the application container already has one of the keys we are trying to add
-    condition = anytrue([
-      for key, value in lookup(var.application_container, "environment", {}) :
-      contains(keys(local.auto_instrumentation_for_app_container_injection_extra_options["js"]), key)
-    ])
-    error_message = "Your application already has an env var that is used by the auto-instrumentation. Contact Team Utviklerplatform for help."
-  }
-
-  precondition {
-    # Do the same for extra options
-    condition = anytrue([
-      for key, value in lookup(var.application_container, "extra_options", {}) :
-      contains(keys(local.auto_instrumentation_for_app_container_injection_extra_options["js"]), key)
-    ])
-    error_message = "Your application already has an extra option that is used by the auto-instrumentation. Contact Team Utviklerplatform for help."
-  }
+#   precondition {
+#     # Error if the application container already has one of the keys we are trying to add
+#     condition = anytrue([
+#       for key, value in lookup(var.application_container, "environment", {}) :
+#       contains(keys(local.auto_instrumentation_for_app_container_injection_extra_options["js"]), key)
+#     ])
+#     error_message = "Your application already has an env var that is used by the auto-instrumentation. Contact Team Utviklerplatform for help."
+#   }
+#
+#   precondition {
+#     # Do the same for extra options
+#     condition = anytrue([
+#       for key, value in lookup(var.application_container, "extra_options", {}) :
+#       contains(keys(local.auto_instrumentation_for_app_container_injection_extra_options["js"]), key)
+#     ])
+#     error_message = "Your application already has an extra option that is used by the auto-instrumentation. Contact Team Utviklerplatform for help."
+#   }
 }
 
 output "init_container_definition" {
