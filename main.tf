a38dedea17263d17aefd505750f14982ffdbdbef
Wictor Zhao <wictor_x@hotmail.com>
2025-07-21 11:38:47 +0200

    Revert "Temporarily comment out ignore changes"
    This reverts commit ba90ce52e09bc65b5668da413d49418e699aada8.

--- main.tf
+++ main.tf
@@ -391,7 +391,7 @@ resource "aws_lb_listener_rule" "service" {
       # Relevant issue: https://github.com/hashicorp/terraform/issues/26359#issuecomment-2578078480
       # Can cause issues if migrating from an older version, using this module
       # "The ELB could not be updated due to the following error: Primary taskset target group must be behind listener"
-      # action[0]
+      action[0]
     ]
   }
 }
@@ -524,7 +524,7 @@ resource "aws_lb_listener_rule" "replacement" {
       #       We can not reference the target groups directly.
       #       So here we are just blanket ignoring the whole forward block and hoping it is OK.
       # Relevant issue: https://github.com/hashicorp/terraform/issues/26359#issuecomment-2578078480
-      # action[0]
+      action[0]
     ]
   }
 }
@@ -967,7 +967,7 @@ resource "aws_ecs_service" "service" {
   }
 
   lifecycle {
-    # ignore_changes = [task_definition, desired_count]
+    ignore_changes = [task_definition, desired_count]
     precondition {
       condition     = !(length(var.placement_constraints) > 0 && var.launch_type == "FARGATE")
       error_message = "Placement constraints are not valid for FARGATE launch type"
