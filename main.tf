6d24a893310a7d72a5067b1984027151b7ae7d05
Anders Rebner <anders.rebner@knowit.no>
2025-09-10 10:02:01 +0200

    Simplify extra_ports statement and add explaining comment

--- main.tf
+++ main.tf
@@ -680,7 +680,8 @@ locals {
     environment   = var.datadog_instrumentation_runtime == null ? {} : module.autoinstrumentation_setup[0].new_environment
     secrets       = merge(module.env_vars_to_ssm_parameters.ssm_parameter_arns, var.application_container.secrets_from_ssm)
     extra_options = merge(try(module.autoinstrumentation_setup[0].new_extra_options, {}), var.application_container.extra_options)
-    extra_ports   = contains([var.application_container.port, "no value"], try(var.lb_health_check.port, "no value")) ? [] : [var.lb_health_check.port]
+    # Extra ports are needed in cases where the Load Balancer Health Check port is different from the application containers normal ports
+    extra_ports = try(var.lb_health_check.port, null) != var.application_container.port ? [var.lb_health_check.port] : []
     }
   )
   init_container = var.datadog_instrumentation_runtime == null ? [] : [module.autoinstrumentation_setup[0].init_container_definition]
